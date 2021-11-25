8fcc17915b97fad3f3488e69ab992e33dee68b51
alexrad10 <72946081+alexrad10@users.noreply.github.com>
2021-11-25 14:19:45 +0300

    Some changes in my_method.py

--- my_method.py
+++ my_method.py
@@ -1,3 +1,9 @@
+N = int(input())  # size of massive
+new_mass = []
+for i in range(N):
+	new_mass.append(int(input()))
+val = find_max(new_mass)
+
 def find_max (mass):
 	temp = mass[0]
 	for element in mass:
